e277f72fca89ba6c1a2aeea18e152d3c5ca19a7c
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2021-02-28 18:33:12 +0100

    Add click listener. Not complete and still buggy.

--- js/main.js
+++ js/main.js
@@ -2,26 +2,47 @@ const ROWS = 8;
 const COLS = 8;
 
 // https://2ality.com/2020/01/enum-pattern.html
-const Piece = {BLACK: 'blackPiece',WHITE:'whitePiece',EMPTY: ''}
+const Piece = {BLACK: 'blackPiece',WHITE:'whitePiece',EMPTY: 'empty'}
 
 const makeRow = () => new Array(COLS).fill(Piece.EMPTY);
 
+let selected = null;
+
 class Cell
 {
- row;
- col;
-
- onclick()
- {
-   console.log("You clicked "+this.col+" "+this.row);
+  onclick()
+  {
+    const piece = this.board.cells[this.row][this.col];
+	this.piece = piece;
+    console.log("You clicked "+this.col+" "+this.row+" with content "+this.piece);
+    if(selected)
+    {
+      console.log("moving");
+      this.piece = selected.piece;
+      this.element.classList.add(selected.piece);
+      this.board.cells[this.row][this.col]=selected.piece;
+      this.board.cells[selected.row][selected.col]=Piece.EMPTY;
+	  selected.element.classList.remove(selected.piece);
+      selected.piece = Piece.EMPTY;
+      selected.element.classList.remove("selected");
+      selected = null;
+    }
+    else
+    { 
+      if(this.piece===Piece.EMPTY) {return;}
+      this.element.classList.add("selected");
+      selected = this;
+   }
  }
 
- constructor(col,row)
- {
-   this.row=row;
-   this.col=col;
-   this.onclick=this.onclick.bind(this);
- }
+  constructor(col,row,board,element)
+  {
+    this.row=row;
+    this.col=col;
+    this.board=board;
+    this.element=element;
+    this.onclick=this.onclick.bind(this);
+  }
 }
 
 class Board
@@ -47,16 +68,16 @@ class Board
       for(let col = 0; col < COLS; col++)
       {
         const cellDiv = document.createElement("div");
-		const cell = new Cell(col,row);
+        const cell = new Cell(col,row,this,cellDiv);
         cellDiv.addEventListener("click",cell.onclick);
-		this.element.appendChild(cellDiv);
+        this.element.appendChild(cellDiv);
         cellDiv.classList.add("cell");
         cellDiv.classList.add(["blackCell","whiteCell"][(col+row)%2]);
-		switch(this.cells[row][col])
-		{
-		  case Piece.BLACK: {cellDiv.classList.add("blackPiece");break;}
-		  case Piece.WHITE: {cellDiv.classList.add("whitePiece");}
-		}
+        switch(this.cells[row][col])
+        {
+          case Piece.BLACK: {cellDiv.classList.add("blackPiece");break;}
+          case Piece.WHITE: {cellDiv.classList.add("whitePiece");}
+        }
 
       }
     }
