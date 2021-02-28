c6b9ce757f1d0fc98e53bd41316bf3a2dc74838e
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2021-02-28 17:34:39 +0100

    Display board in JavaScript.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,27 @@ const ROWS = 8;
 const COLS = 8;
 
 // https://2ality.com/2020/01/enum-pattern.html
-const Cell = {BLACK: 'B',WHITE:'W',EMPTY: '_'}
+const Piece = {BLACK: 'blackPiece',WHITE:'whitePiece',EMPTY: ''}
 
-const makeRow = () => new Array(COLS).fill(Cell.EMPTY);
+const makeRow = () => new Array(COLS).fill(Piece.EMPTY);
+
+class Cell
+{
+ row;
+ col;
+
+ onclick()
+ {
+   console.log("You clicked "+this.col+" "+this.row);
+ }
+
+ constructor(col,row)
+ {
+   this.row=row;
+   this.col=col;
+   this.onclick=this.onclick.bind(this);
+ }
+}
 
 class Board
 {
@@ -12,16 +30,16 @@ class Board
   cells = Array.from({length: ROWS}, makeRow)
 
   element = document.createElement("div");
-
+  x = 5;
   constructor()
   {
-    element.classList.add("board");
-    for(let row = 0; row < ROWS; row+=2)
+    this.element.classList.add("board");
+    for(let row = 0; row < 2; row++)
     {
-      for(let col = 0; col < 2; col++)
+      for(let col = 0; col < COLS; col+=2)
       {
-        this.cells[row+col][col]=Cell.BLACK;
-        //this.cells[ROWS-row+col][col]=Cell.WHITE;
+        this.cells[row][col+row]=Piece.BLACK;
+        this.cells[ROWS-1-row][col+1-row]=Piece.WHITE;
       }
     }
     for(let row = 0; row < ROWS; row++)
@@ -29,10 +47,16 @@ class Board
       for(let col = 0; col < COLS; col++)
       {
         const cellDiv = document.createElement("div");
-        this.element.appendChild(cellDiv);
+		const cell = new Cell(col,row);
+        cellDiv.addEventListener("click",cell.onclick);
+		this.element.appendChild(cellDiv);
         cellDiv.classList.add("cell");
         cellDiv.classList.add(["blackCell","whiteCell"][(col+row)%2]);
-        cellDiv.classList.add("blackPiece");
+		switch(this.cells[row][col])
+		{
+		  case Piece.BLACK: {cellDiv.classList.add("blackPiece");break;}
+		  case Piece.WHITE: {cellDiv.classList.add("whitePiece");}
+		}
 
       }
     }
